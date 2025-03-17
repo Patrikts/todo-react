5959e7311ac276449882c509763c9e04096abc79
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2025-03-17 15:29:12 -0300

    Update TodoForm.jsx

--- todo/src/components/TodoForm.jsx
+++ todo/src/components/TodoForm.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoForm = () => {
+    const [value, setValue] = useState("");
+    const [category, setCategory] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(value, category);
+    }
+
   return ( <div className='todo-form'>
     <h2>Criar tarefa:</h2>
-    <form>
-        <input type="text" placeholder='Digite o título' />
-        <select>
+    <form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        placeholder='Digite o título' 
+        onChange={(e) => setValue(e.target.value)}
+        />
+        <select onChange={(e) => setCategory(e.target.value)}>
             <option value="">Selecione uma categoria</option>
             <option value="">Trabalho</option>
             <option value="">Pessoal</option>
