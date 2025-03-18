81273056b47ad84ba6186454a8011e9c8fba6727
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2025-03-18 15:38:04 -0300

    update project

--- todo/src/components/Search.jsx
+++ todo/src/components/Search.jsx
@@ -1,12 +1,15 @@
-const Search = ({ search, setSearch }) => {
-  return (<div className="search">
-    <h2>Pesquisar</h2>
-    <input type="text" 
-    value={search} 
-    onChange={(e) => setSearch(e.target.value)} 
-    placeholder="Digite para pesquisar..."/>
+import React from "react";
+
+const Search = ({ search, setSearch }) => (
+  <div className="search">
+    <h2>Pesquisar:</h2>
+    <input
+      type="text"
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+      placeholder="Digite para pesquisar..."
+    />
   </div>
-  );
-};
+);
 
 export default Search;
--- todo/src/components/TodoForm.jsx
+++ todo/src/components/TodoForm.jsx
@@ -1,36 +1,37 @@
-import React, { useState } from 'react'
+import { useState } from "react";
 
 const TodoForm = ({ addTodo }) => {
-    const [value, setValue] = useState("");
-    const [category, setCategory] = useState("");
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!value || !category) return;
-        addTodo(value, category);
-        setValue("");
-        setCategory("");
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value, category);
+    setValue("");
+    setCategory("");
+  };
 
-  return ( 
-  <div className='todo-form'>
-    <h2>Criar tarefa:</h2>
-    <form onSubmit={handleSubmit}>
-        <input 
-            type="text" 
-            placeholder='Digite o título' 
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+  return (
+    <div className="todo-form">
+      <h2>Criar tarefa:</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          value={value}
+          placeholder="Digite o título"
+          onChange={(e) => setValue(e.target.value)}
         />
         <select value={category} onChange={(e) => setCategory(e.target.value)}>
-            <option value="">Selecione uma categoria</option>
-            <option value="">Trabalho</option>
-            <option value="">Pessoal</option>
-            <option value="">Estudos</option>
+          <option value="">Seleciona uma categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Pessoal">Pessoal</option>
+          <option value="Estudos">Estudos</option>
         </select>
         <button type="submit">Criar tarefa</button>
-    </form>
-  </div>
+      </form>
+    </div>
   );
 };
 
