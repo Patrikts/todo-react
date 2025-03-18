6317552b53d59f122ddd7ffdaa07c161d40e148f
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2025-03-18 14:17:47 -0300

    Criado componente Filtrar:

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -5,6 +5,7 @@ import TodoForm from './components/TodoForm';
 import Search from './components/Search';
 
 import "./App.css";
+import Filter from './components/Filter';
 
 function App() {
 const [todos, setTodos] = useState([
@@ -62,6 +63,7 @@ const [search, setSearch] = useState("")
   return <div className='app'>
     <h1>Lista de Tarefas</h1>
     <Search search={search} setSearch={setSearch} />
+    <Filter />
     <div className="todo-list">
       {todos.filter((todo) => 
       todo.text.toLowerCase().includes(search.toLowerCase()))
